92ffefb4b0d17934b4384a04acb4d30ee8f28ef1
tgk96 <kanuj96@gmail.com>
2017-05-31 18:20:09 +0530

    Corrected some minor errors in Login Activity Commented RegisterActivity.java to make it work git commit -m

--- app/src/main/java/com/example/yoh/one4all/RegisterActivity.java
+++ app/src/main/java/com/example/yoh/one4all/RegisterActivity.java
@@ -13,17 +13,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }
